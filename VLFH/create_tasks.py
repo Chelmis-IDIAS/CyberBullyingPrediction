da454a2b16ae4ae2bbf95405e011d5098f7cdf86
Chelmis-IDIAS <47668915+Chelmis-IDIAS@users.noreply.github.com>
2019-02-15 11:31:32 -0500

    Add files via upload

---
+++ VLFH/create_tasks.py
@@ -0,0 +1,110 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+from __future__ import division
+import pandas as pd
+import collections
+import numpy as np
+import string
+from nltk.stem import PorterStemmer
+from nltk.stem import WordNetLemmatizer
+from sklearn.model_selection import train_test_split
+from imblearn.over_sampling import RandomOverSampler,SMOTE
+import vaderSentiment
+import re
+import datetime
+from dateutil.relativedelta import relativedelta
+import csv
+from sklearn.model_selection import train_test_split
+
+def extract_textfeatures_flavor2(comment):
+    words = ['fuck', 'bitch', 'shut', 'hate', 'suck', 'gay', 'ugli', 'work', 'beauti', 'sick']
+    analyzer = vaderSentiment.SentimentIntensityAnalyzer()
+    vader = analyzer.polarity_scores(comment).get('compound')
+    f = pd.read_csv('Terms-to-Block.csv')['0'].tolist()
+    ps = PorterStemmer()
+    wordnet_lemmatizer = WordNetLemmatizer()
+    words = [ps.stem(wordnet_lemmatizer.lemmatize(w)) for w in words]
+    comment = unicode(comment, errors='ignore')
+    comment = comment.decode('utf-8').encode('ascii', errors='ignore')
+    punc = sum([comment.count(i) for i in ["!", "?"]])
+    url =len(re.findall('https?://(?:[-\w.]|(?:%[\da-fA-F]{2}))+', comment))
+    user = len(re.findall(r'(?<=^|(?<=[^a-zA-Z0-9-_\.]))@([A-Za-z]+[A-Za-z0-9-_]+)', comment))
+    hashtag = len(re.findall(r"#(\w+)", comment))
+    comment = ' '.join(re.sub("(@[A-Za-z0-9]+)|([^0-9A-Za-z \t])|(\w+:\/\/\S+)"," ",comment).split())
+    textlen = len(comment.split())
+    if textlen ==0:
+        textlen =1
+    punc = punc/textlen
+    uppercase = len(filter(lambda x: x in string.uppercase, comment)) / textlen
+    comment = [x.lower() for x in comment.split()]
+    comment = [ps.stem(wordnet_lemmatizer.lemmatize(w)) for w in comment]
+    comment = " ".join(comment)
+    badwords = sum([comment.count(i) for i in f])/textlen
+    # for uni in [comment.count(x) for x in words]:
+    #     return uni
+    unigrams = [comment.count(x) for x in words]
+    all_features = tuple([textlen,punc,uppercase,badwords,vader,hashtag,user,url]+unigrams)
+
+    # return textlen,punc,uppercase,badwords,vader,hashtag,user,url,tuple(unigrams)
+    return all_features
+#
+# print extract_textfeatures_flavor2("are you fucking stupid? shut up ugly bitch!!!")
+
+# def divide_tasks(k,tau,task):
+#     #input k: number of initial give comments
+#     #input tau: time lag
+#     #note: data X_i is the same across all tasks, need to generate target set Y
+#     X = pd.read_csv("")
+# def create_tasks(tau,k,)
+tau = 1
+k = 20
+step  = 2
+max_lenth = 148 # max number of comments
+
+import os
+directory = 'tau='+str(tau)+'_k='+str(k) +'_step='+str(step)
+if not os.path.exists(directory):
+    os.makedirs(directory)
+
+data = pd.read_csv('raw_text_labeled.csv').iloc[:,1:]
+D = pd.DataFrame()
+for t in range(int((max_lenth-k)/tau))[:10]:
+    # print t
+    X = []
+    Y = []
+    for i in range(204)[:]:
+        print t,i
+        X_i = []
+        session_i = data[data['idx'] == i]
+        len_session_i = len( data[data['idx'] == i])
+        # max_t = int((len_session_i-k)/tau)
+
+        # for t in range(1,max_t+1):
+
+        for j in range(len_session_i-tau-(k+step*t)+1):
+            window = session_i.iloc[j:j+k+step*t]['comment']#.values.flatten().tolist()
+
+            window = ' '.join([x.split('(created at')[0] for x in window])
+            X.append(list(extract_textfeatures_flavor2(window))+[i])
+            x_task_t = window
+            list_y = [1 if x == 'T' else 0 for x in session_i.iloc[j:j + k + t * step + tau]['label'].tolist()]
+            Y.append(list_y)
+        # print pd.DataFrame(X)
+    if len(X)>0:
+        df = pd.concat((pd.DataFrame(X),pd.DataFrame(Y).sum(axis =1)),axis =1)
+        df.columns = ['textlen','punc','uppercase','badwords','vader','hashtag','user','url',
+                  'fuck', 'bitch', 'shut', 'hate', 'suck', 'gay', 'ugli', 'work', 'beauti', 'sick','idx','label']
+        # print df
+        # df.to_csv(directory+'/task_'+str(t)+str('.csv'),index = False)
+        if not os.path.exists(directory + '/train'):
+            os.makedirs(directory + '/train')
+        if not os.path.exists(directory + '/test'):
+            os.makedirs(directory + '/test')
+        x_train, x_test, y_train, y_test, = train_test_split(
+            df.iloc[:,:-2], df['label'], test_size=0.3, random_state=42)
+
+        x_train['label'] = y_train
+        x_test['label'] = y_test
+        # print x_train,x_test
+        x_train.to_csv(directory + '/train/task_' + str(t) + '.csv', index=False)
+        x_test.to_csv(directory + '/test/task_' + str(t) + '.csv', index=False)
---
+++ VLFH/extract_features.py
@@ -0,0 +1,115 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+from __future__ import division
+import pandas as pd
+import collections
+import numpy as np
+import string
+from nltk.stem import PorterStemmer
+from nltk.stem import WordNetLemmatizer
+from sklearn.model_selection import train_test_split
+from imblearn.over_sampling import RandomOverSampler,SMOTE
+import vaderSentiment
+import re
+import datetime
+from dateutil.relativedelta import relativedelta
+import csv
+def extract_textfeatures_flavor2(comment):
+    words = ['fuck', 'bitch', 'shut', 'hate', 'suck', 'gay', 'ugli', 'work', 'beauti', 'sick']
+    analyzer = vaderSentiment.SentimentIntensityAnalyzer()
+    vader = analyzer.polarity_scores(comment).get('compound')
+    f = pd.read_csv('Terms-to-Block.csv')['0'].tolist()
+    ps = PorterStemmer()
+    wordnet_lemmatizer = WordNetLemmatizer()
+    words = [ps.stem(wordnet_lemmatizer.lemmatize(w)) for w in words]
+    comment = unicode(comment, errors='ignore')
+    comment = comment.decode('utf-8').encode('ascii', errors='ignore')
+    punc = sum([comment.count(i) for i in ["!", "?"]])
+    url =len(re.findall('https?://(?:[-\w.]|(?:%[\da-fA-F]{2}))+', comment))
+    user = len(re.findall(r'(?<=^|(?<=[^a-zA-Z0-9-_\.]))@([A-Za-z]+[A-Za-z0-9-_]+)', comment))
+    hashtag = len(re.findall(r"#(\w+)", comment))
+    comment = ' '.join(re.sub("(@[A-Za-z0-9]+)|([^0-9A-Za-z \t])|(\w+:\/\/\S+)"," ",comment).split())
+    textlen = len(comment.split())
+    if textlen ==0:
+        textlen =1
+    punc = punc/textlen
+    uppercase = len(filter(lambda x: x in string.uppercase, comment)) / textlen
+    comment = [x.lower() for x in comment.split()]
+    comment = [ps.stem(wordnet_lemmatizer.lemmatize(w)) for w in comment]
+    comment = " ".join(comment)
+    badwords = sum([comment.count(i) for i in f])/textlen
+    # for uni in [comment.count(x) for x in words]:
+    #     return uni
+    unigrams = [comment.count(x) for x in words]
+    all_features = tuple([textlen,punc,uppercase,badwords,vader,hashtag,user,url]+unigrams)
+
+    # return textlen,punc,uppercase,badwords,vader,hashtag,user,url,tuple(unigrams)
+    return all_features
+
+print extract_textfeatures_flavor2("are you fucking stupid? shut up ugly bitch!!!")
+
+def extract_temporalfeatures_flavor2(comments,idx):
+    #input list of comments and the index of session they belong to
+    data = pd.read_csv('sessions_40plus_metadata.csv')['cptn_time'].tolist()[idx]
+    t0_str = '2'+data.strip('Media posted at ')
+    t0 = datetime.datetime.strptime(t0_str,'%Y-%m-%d %H:%M:%S') 
+    ct= []
+    for comment in comments:
+        end = datetime.datetime.strptime(comment.split('created at:')[-1].strip(')'),'%Y-%m-%d %H:%M:%S')
+        diff = relativedelta(end,t0)
+        diff_hour = diff.years*8760+diff.months*730+diff.days*24+diff.hours+diff.minutes*0.017
+        ct.append(diff_hour)
+    if len(ct)>2:
+        delta_t=[round(x - ct[i - 1],3) for i, x in enumerate(ct)]
+    else:
+        delta_t = ct
+    time2first = ct[0]
+    ICI_mean = np.mean(delta_t)+0.0001
+    ICI_var = np.std(delta_t)**2
+    ICI_coef = np.std(delta_t)/ICI_mean
+    x = ct
+    t_N = max(x)
+    line = range(int(t_N))
+    at= 0
+    for t in line:
+       if sum([ t - xx for xx in x if xx < t ])!=0:
+            at+= sum([ np.exp(-2*(t - xx)) for xx in x if xx < t ])
+    at_mean = np.mean(at)
+    return time2first,ICI_mean,ICI_var,ICI_coef,at,at_mean
+
+# print extract_temporalfeatures_flavor2(['Cool nail but I dont bite on mine (created at:2012-08-07 03:46:09)', '@offthis_ way to be a asshole (created at:2012-11-24 03:57:26)'],0)
+
+D = pd.DataFrame()
+Y = []
+I= []
+labeled_data = pd.read_csv('raw_text_labeled.csv')
+idx = labeled_data['idx'].tolist()
+
+# for i in list(set(idx))[:]:
+#     session_i = labeled_data[labeled_data['idx']==i]
+#     l = labeled_data[labeled_data['idx']==i]['label'].tolist()
+#     for j in range(len(session_i)):
+#         first_j_comments = session_i['comment'].iloc[:j+1].values.flatten().tolist()
+#         # Temporal = extract_temporalfeatures_flavor2(first_j_comments,int(i))
+#         first_j_comments = ' '.join([x.split('(created at')[0] for x in first_j_comments])
+#         Textual = extract_textfeatures_flavor2(first_j_comments)
+#         print i,j,[l[:j+1].count('T')/5 if l[:j+1].count('T')<=5 else 1][0]
+#         file = open('features/flavor2/textual+unigrams/train/task' + str(j) + '.csv','a+')
+#         csv.writer(file, lineterminator='\n').writerow((i,[l[:j+1].count('T')/5 if l[:j+1].count('T')<=5 else 1][0])+Textual)
+#         file.close()
+
+#
+#
+unlabeled_data = pd.read_csv('raw_text_unlabeled.csv')
+idx = unlabeled_data['idx'].tolist()
+for i in list(set(idx)):
+    session_i = unlabeled_data[unlabeled_data['idx']==i]
+    for j in range(len(session_i))[:]:
+            first_j_comments = session_i['comment'].iloc[:j+1].values.flatten().tolist()
+            # Temporal = extract_temporalfeatures_flavor2(first_j_comments,int(i))
+            first_j_comments = ' '.join([x.split('(created at')[0] for x in first_j_comments])
+            Textual = extract_textfeatures_flavor2(first_j_comments)
+            print i,j
+            file = open('features/flavor2/textual+unigrams/test/task' + str(j) + '_alltasks.csv','a+')
+            csv.writer(file, lineterminator='\n').writerow((i,)+Textual)
+            file.close()
